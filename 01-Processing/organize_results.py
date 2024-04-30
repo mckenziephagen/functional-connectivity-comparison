c56daec6cadc980c6eec2932c7bc3f788e26d2ba
mckenziephagen <mphagen@uw.edu>
2024-04-29 20:55:36 -0700

    rename files, write fucntions for organizing_results.py

--- 01-Processing/organize_results.py
+++ 01-Processing/organize_results.py
@@ -23,81 +23,96 @@ import matplotlib.pyplot as plt
 
 # +
 fc_data_path = '/pscratch/sd/m/mphagen/hcp-functional-connectivity'
+organized_result_files = '/global/homes/m/mphagen/functional_connectivity_comparison/results'
 
+data_str = str(datetime.date.today())
 
-pearson_results_path = op.join(fc_data_path, 'derivatives', f'fc-matrices_schaefer*', '*', '*correlation*')
-lasso_results_path = op.join(fc_data_path, 'derivatives', f'fc-matrices_schaefer*', '*', '*fc-lasso*model*')
-uoi_results_path = op.join(fc_data_path, 'derivatives', f'fc-matrices_schaefer*', '*', '*uoi-*model*')
-# -
+fc_matrices_path = op.join(fc_data_path, 
+                           'derivatives', 
+                           'fc-matrices_schaefer-100')
 
-uoi_r2_path = op.join(fc_data_path, 'derivatives', f'fc-matrices_schaefer*', '*', '*uoi-*r2*')
+pearson_model_path = op.join(fc_matrices_path, 
+                             '*',
+                             '*fc-correlation.pkl')
 
-pearson_results_files = glob(pearson_results_path) 
-lasso_result_files = glob(lasso_results_path)
-pyuoi_result_files = glob(uoi_results_path) 
+lasso_model_path = op.join(fc_matrices_path, 
+                             '*', 
+                             'lasso-bic', 
+                             '*fc-lasso-bic_model.pkl')
 
-pyuoi_r2_files = glob(uoi_r2_path)
-
-len(pearson_results_files)
-
-len(lasso_result_files) 
-
-lasso_dict = {} 
-for idx, file in enumerate(lasso_result_files): 
-    with open(file, 'rb') as f:
-         mat = pickle.load(f)    
-    ses_id = '_'.join(file.split('/')[-1].split('_')[-4:-2])
-    sub_id = file.split('/')[-2]
-
-    if sub_id not in lasso_dict.keys(): 
-        lasso_dict[sub_id] = {} 
-    #average five folds together
-    lasso_dict[sub_id].update({ses_id: np.median(np.array([*mat.values()]), axis=0)})
-#now I have a dictionary of lasso fc matrices averaged over five folds  
-with open(op.join('results', f'{str(datetime.date.today())}_lasso_dict.pkl'), 'wb') as f:
-        pickle.dump(lasso_dict, f)
-
-pearson_dict = {} 
-for idx, file in enumerate(pearson_results_files): 
-    with open(file, 'rb') as f:
-         mat = pickle.load(f)    
-    ses_id = '_'.join(file.split('/')[-1].split('_')[-3:-1])
-    sub_id = file.split('/')[-2]
-
-    if sub_id not in pearson_dict.keys(): 
-        pearson_dict[sub_id] = {} 
-    #average five folds together
-    pearson_dict[sub_id].update({ses_id: mat})
-with open(op.join('results', f'{str(datetime.date.today())}_pearson_dict.pkl'), 'wb') as f:
-            pickle.dump(pearson_dict, f)
-
+uoi_model_path = op.join(fc_matrices_path, 
+                           '*',
+                           '*fc-uoi-lasso_model.pkl')
 
 # +
-uoi_dict = {} 
-for idx, file in enumerate(pyuoi_result_files): 
-    with open(file, 'rb') as f:
-         mat = pickle.load(f)    
-    ses_id = '_'.join(file.split('/')[-1].split('_')[-4:-2])
-    sub_id = file.split('/')[-2]
+uoi_r2_path = op.join(fc_matrices_path,
+                       '*', 
+                       '*fc-uoi-lasso_r2.pkl')
 
-    if sub_id not in uoi_dict.keys(): 
-        uoi_dict[sub_id] = {} 
-    #average five folds together
-    uoi_dict[sub_id].update({ses_id: np.median(np.array([*mat.values()]), axis=0)})
-    
-with open(op.join('results', f'{str(datetime.date.today())}_uoi_dict.pkl'), 'wb') as f:
-        pickle.dump(uoi_dict, f)
+lasso_r2_path = op.join(fc_matrices_path,
+                        '*', 
+                        'lasso-bic',
+                        '*fc-lasso-bic_r2.pkl')
 # -
 
-pearson_icc_df = pd.DataFrame(columns=['values', 'ses', 'sub'] )
+pearson_files = glob(pearson_model_path) 
+lasso_model_files = glob(lasso_model_path)
+uoi_model_files = glob(uoi_model_path) 
+
+print(len(pearson_files)) 
+print(len(lasso_model_files)) 
+print(len(pyuoi_model_files)) 
+
+uoi_r2_files = glob(uoi_r2_path)
+lasso_r2_files = glob(lasso_r2_path)
+
+print(len(uoi_r2_files))
+print(len(lasso_r2_files))
+
+
+def unpack_model_results(result_files, result_string): 
+    result_dict = {} 
+    for idx, file in enumerate(result_files): 
+      #  print(file) 
+        with open(file, 'rb') as f:
+             mat = pickle.load(f)    
+        ses_id = '_'.join(file.split('/')[-1].split('_')[3:5])
+        sub_id = file.split('/')[-1].split('_')[0]
+        
+        if sub_id not in result_dict.keys(): 
+            result_dict[sub_id] = {} 
+            
+        if result_string == 'pearson': 
+            result_dict[sub_id].update({ses_id: mat})
+        else: 
+            result_dict[sub_id].update({ses_id: 
+                                        np.median(np.array([*mat.values()]), 
+                                                  axis=0)})
+
+        with open(op.join(organized_result_files,
+                          f'{data_str}_{result_string}_dict.pkl'), 'wb') as f:
+            pickle.dump(result_dict, f)
+            
+    return(result_dict) 
+
+
+lasso_dict = unpack_model_results(lasso_model_files, 'lasso-bic') 
+
+uoi_dict = unpack_model_results(uoi_model_files, 'uoi') 
+
+pearson_dict = unpack_model_results(pearson_files, 'pearson') 
+
+pearson_dict.values() 
+
+pearson_icc_df = pd.DataFrame(columns=['values', 'ses', 'sub', 'pos'] )
 for outer in pearson_dict.keys():
     for inner in pearson_dict[outer].keys():
         temp_df = pd.DataFrame(data = {'values': pearson_dict[outer][inner].ravel(), 
-                               'sub':  outer, 
-                              'ses': inner, 
-                            'pos': list(range(1,10001,1))})
+                                       'sub':  outer, 
+                                       'ses': inner, 
+                                       'pos': list(range(1,10001,1))})
         pearson_icc_df = pd.concat([pearson_icc_df, temp_df])
-pearson_icc_df.to_csv('pearson_icc_df.csv')
+pearson_icc_df.to_csv(op.join(organized_result_path, 'pearson_icc_df.csv'))
 
 lasso_icc_df = pd.DataFrame(columns=['values', 'ses', 'sub', 'pos'] )
 for outer in lasso_dict.keys():
@@ -109,8 +124,6 @@ for outer in lasso_dict.keys():
         lasso_icc_df = pd.concat([lasso_icc_df, temp_df])
 lasso_icc_df.to_csv('lasso_icc_df.csv')
 
-uoi_dict[outer][inner].shape
-
 # +
 uoi_icc_df = pd.DataFrame(columns=['values', 'ses', 'sub'] )
 for outer in uoi_dict.keys():
@@ -121,21 +134,31 @@ for outer in uoi_dict.keys():
         uoi_icc_df = pd.concat([uoi_icc_df, temp_df])
         
 uoi_icc_df.to_csv('uoi_icc_df.csv')
-# -
 
-len(uoi_dict)
-
-lasso_dict.keys() 
-
-len(pearson_dict)
-
-r2_mean_list = []
+# +
+r2_lasso = []
+for i in lasso_r2_files: 
+    with open(i, 'rb') as f:
+         mat = pickle.load(f)  
+    r2_df = pd.json_normalize(mat).filter(like='test')
+    
+    r2_lasso.append(np.mean(r2_df.explode(list(r2_df.columns))))
+    
+    
+r2_pyuoi = []
 for i in pyuoi_r2_files: 
     with open(i, 'rb') as f:
          mat = pickle.load(f)  
     r2_df = pd.json_normalize(mat).filter(like='test')
     
-    r2_mean_list.append(np.mean(r2_df.explode(list(r2_df.columns))))
+    r2_pyuoi.append(np.mean(r2_df.explode(list(r2_df.columns))))
+# +
+#this is weird
+plt.hist(r2_lasso) 
+plt.hist(r2_pyuoi) 
+
+plt.title('Histogram of UoI and Lasso Accuracies') 
+# -
 
 #I shoudl investigate those lower ones and cut them
 plt.hist(r2_mean_list)
