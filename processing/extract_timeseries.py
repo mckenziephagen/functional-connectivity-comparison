e793f84a697c9d473c6e8b68770776189c9a0ee3
mckenziephagen <mphagen@uw.edu>
2024-11-18 09:07:56 -0800

    create function to save timeseries

--- processing/extract_timeseries.py
+++ processing/extract_timeseries.py
@@ -32,7 +32,7 @@ import os.path as op
 
 sys.path.append(os.path.dirname('../fc_comparison'))
 
-from fc_comparison.main import parcellate_data
+from fc_comparison.files import parcellate_data
 # -
 
 
@@ -47,8 +47,9 @@ parser = argparse.ArgumentParser("extract_timeseries.py")
 parser.add_argument('--subject_id',  default='101107') 
 parser.add_argument('--atlas_name', default='schaefer')
 parser.add_argument('--n_rois', default=100)
-parser.add_argument('--resolution_mm', default=1) #I don't remember where I got this #
+parser.add_argument('--resolution_mm', default=1) 
 parser.add_argument('--yeo_networks', default=7)
+parser.add_argument('--dataset', default='HCP') 
 
 #hack argparse to be jupyter friendly AND cmdline compatible
 try: 
@@ -65,23 +66,55 @@ yeo_networks = args.yeo_networks
 
 print(args)
 # +
-fc_data_path = '/pscratch/sd/m/mphagen/hcp-functional-connectivity'
-results_path = op.join(fc_data_path, 
-                       'derivatives', 
-                       'parcellated-timeseries', 
-                        f'sub-{subject_id}', 
-                        'func')
-os.makedirs(results_path, exist_ok=True)
+def define_paths(path_prefix, dataset, results_str): 
+    """
+    DOCSTRING
+    """
+    if dataset == 'HCP': 
+        dataset_path = op.join(path_prefix, 'hcp-functional-connectivity') 
+   
+    derivatives_path = op.join(dataset_path, 'derivatives') 
+    results_dir = op.join(derivatives_path, results_str)
 
-rest_scans = glob(op.join(fc_data_path, 
+    path_dict = {'dataset_path': dataset_path, 
+                 'derivatives_path': derivatives_path,
+                 'results_dir': results_dir}
+    
+    
+    return path_dict
+        
+        
+def save_parcellated_ts(results_dir, subject_id, ses_string, atlas_name, n_rois): #make this a dictionary
+     
+    tsv_path = op.join(results_dir, 
+                       ses_string, 
+                       f'{atlas_name}-{n_rois}')
+                       
+    os.makedirs(tsv_path, exist_ok=True)
+    
+    file_str =  f'sub-{subject_id}_{ses_string}_task-Rest_atlas-{atlas_name}{n_rois}_timeseries.tsv'
+                
+    ts.tofile(op.join(tsv_path, file_str), 
+              sep = '\t')
+        
+
+
+# +
+path_dict = define_paths('/pscratch/sd/m/mphagen', 'HCP', 'parcellated-timeseries')
+                       
+os.makedirs(path_dict['results_dir'], exist_ok=True)
+
+rest_scans = glob(op.join(path_dict['dataset_path'], 
                           subject_id, 
                           'MNINonLinear/Results/rfMRI*', 
                           '*clean.nii.gz'))
 
+assert len(rest_scans) > 0 
+
 print(f"Found {len(rest_scans)} rest scans for subject {subject_id}") 
 # -
 
-results_path
+path_dict['results_dir']
 
 # +
 #add elif here for other atlas choice
@@ -92,17 +125,26 @@ if atlas_name == 'schaefer':
     atlas = schaefer['maps']
 
 masker = NiftiLabelsMasker(labels_img=atlas, standardize='zscore_sample')
-# -
 
-dataset_path = '/pscratch/sd/m/mphagen/hcp-functional-connectivity'
-
+# +
 for file in rest_scans: 
-    ts, ses_string = parcellate_data(file, dataset_path, 
-                         masker)  
+    ts, ses_string = parcellate_data(file, path_dict['dataset_path'], masker)  
     
+    save_parcellated_ts(path_dict['dataset_path'],
+                        subject_id, 
+                        ses_string, 
+                        atlas_name,
+                        n_rois)
      
-    ts.tofile(op.join(results_path, 
-                       ses_string, 
-                       f'{atlas_name}-{n_rois}'
-                       f'sub-{subject_id}_ses-{ses_string}_task-Rest_atlas-{atlas_name}{n_rois}_timeseries.tsv'), 
-                        sep = '/t')
+#     os.makedirs(op.join(results_path, 
+#                        ses_string, 
+#                        f'{atlas_name}-{n_rois}'), 
+#                 exist_ok=True)
+                
+#     ts.tofile(op.join(results_path, 
+#                        ses_string, 
+#                        f'{atlas_name}-{n_rois}',
+#                        f'sub-{subject_id}_ses-{ses_string}_task-Rest_atlas-{atlas_name}{n_rois}_timeseries.tsv'), 
+#                         sep = '\t')
+    
+    #double check BIDS nroi (does this need to be desc instead?
